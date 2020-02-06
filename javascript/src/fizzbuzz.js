7275f42d2e03999ebc0099eb07eb9bef35672ae1
Dan Putman <57899154+dan-codacy@users.noreply.github.com>
2020-02-06 08:03:35 -0500

    Attempt PR block

--- javascript/src/fizzbuzz.js
+++ javascript/src/fizzbuzz.js
@@ -4,6 +4,7 @@ module.exports = function(input) {
         output += 'Fizz'
     if(input % 5 === 0) 
         output += 'Buzz'
+    if(input % 99 == 0) output += 'Something'
 
     return output.length > 0 ? output : input
 }
