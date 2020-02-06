97d5025ccb40e881f6cdfc50b9f18143331a926a
Dan Putman <57899154+dan-codacy@users.noreply.github.com>
2020-02-06 08:06:46 -0500

    Attempt #2

--- javascript/src/fizzbuzz.js
+++ javascript/src/fizzbuzz.js
@@ -5,6 +5,7 @@ module.exports = function(input) {
     if(input % 5 === 0) 
         output += 'Buzz'
     if(input % 99 == 0) output += 'Something'
+    if(input % 101 == 0) output += 'New'
 
     return output.length > 0 ? output : input
 }
